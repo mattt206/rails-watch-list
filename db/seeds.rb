307bae26070b1e4822984a3d9f733c144700f1df
mateo holguin <mattt206@gmail.com>
2022-09-21 22:52:48 -0500

    cloudinare production change

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,28 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 require 'faker'
+require "json"
+require "open-uri"
 
 Movie.destroy_all
 
-5.times do Movie.create(
-  title: Faker::Movie.title,
-  overview: Faker::Movie.quote,
-  poster_url: Faker::LoremFlickr.image,
-  rating: rand(1.0..10.0).floor(2)
-)
+# 5.times do Movie.create(
+#   title: Faker::Movie.title,
+#   overview: Faker::Movie.quote,
+#   poster_url: Faker::LoremFlickr.image,
+#   rating: rand(1.0..10.0).floor(2)
+# )
+# end
+
+url = "https://tmdb.lewagon.com/movie/top_rated?api_key=%3Ctmdb.lewagon.com%3E"
+movie_serialized = URI.open(url).read
+result = JSON.parse(movie_serialized)
+
+15.times do |num|
+  Movie.create(
+    title: result["results"][num]["title"],
+    overview: result["results"][num]["overview"],
+    poster_url: "https://image.tmdb.org/t/p/w500/#{result['results'][num]['poster_path']}",
+    rating: rand(5.0..10.0).floor(2)
+  )
 end
